b28fab84bae231608df105c733be3024d9378731
amadzia <agnieszkamadzia@onet.pl>
2018-07-23 07:44:45 +0200

    first commit

---
+++ src/test/java/com/example/reactivestockquoteservice/ReactiveStockQuoteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.reactivestockquoteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveStockQuoteServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
